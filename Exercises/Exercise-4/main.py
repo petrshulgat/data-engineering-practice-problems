d91c63dd302e4e7c462f49872c5f6b67285f60f4
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-04-16 22:58:54 +0200

    solved ex4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,9 +1,31 @@
-import boto3
+import json 
+import csv 
+import glob 
+import pandas as pd
+from pathlib import Path 
 
 
 def main():
-    # your code here
-    pass
+    
+    dir = Path('data')
+
+    for file in dir.rglob('*.json'):
+
+        filename = file.with_suffix('.csv')
+
+        with open(file, 'r') as f:
+            data =  json.load(f)
+
+        if isinstance(data, dict):
+            data = [data]
+
+        df = pd.json_normalize(data)
+
+        with open(filename, 'w', newline='') as csvf:
+            writer = csv.DictWriter(csvf, fieldnames=df.columns.tolist())
+            writer.writeheader()
+            writer.writerows(df.to_dict(orient='records'))
+
 
 
 if __name__ == "__main__":
